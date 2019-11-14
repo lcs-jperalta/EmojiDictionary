5422fac98a6e1b1694b2e51281031817e9337953
Joven Peralta <jperalta@lcs.on.ca>
2019-11-13 21:01:16 -0500

    Defined emoji struct.

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,19 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Peralta, Joven on 2019-11-13.
+//  Copyright © 2019 Peralta, Joven. All rights reserved.
+//
+
+import Foundation
+
+// Define the Emoji struct (describe its generic properties, etc)
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
+//
+//var example = Emoji(symbol: "🔰", name: "Japanese symbol for beginner", description: "A symbol used for beginner japanese drivers", usage: "use randomly out of context")
